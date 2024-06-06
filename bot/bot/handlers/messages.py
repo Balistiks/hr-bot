f891d06f37843cd327be330c2e40b2da8a94ee2b
Афанасьев Иван <veiby325@gmail.com>
2024-06-06 19:35:12 +1000

    registration

--- bot/bot/handlers/messages.py
+++ bot/bot/handlers/messages.py
@@ -1,3 +1,4 @@
+import re
 from aiogram import Router, types
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
@@ -18,21 +19,23 @@ async def start_handler(message: types.Message, state: FSMContext):
 async def get_registration_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(RegisterState.phone_number)
-    await message.answer('Введите ваш номер телефона?',
-                         reply_markup=keyboards.registration.SEND_CONTACT)
+    await message.answer('Введите ваш номер телефона?')
 
 
 @messages_router.message(RegisterState.phone_number)
 async def get_registration_phone(message: types.Message, state: FSMContext):
-    await state.update_data(phone_number=message.text)
-    get_data_registration = await state.get_data()
-    await state.clear()
-
-
-@messages_router.message(RegisterState.phone_number)
-async def get_registration_name(message: types.Message, state: FSMContext):
-    await message.answer_photo(
+    if re.match(r'\+79(\d{9})$', message.text):
+        await state.update_data(phone_number=message.text)
+        get_data_registration = await state.get_data()
+        await state.clear()
+        print(get_data_registration)
+        await message.answer_photo(
             photo=types.FSInputFile('files/chatademia.png'),
             caption='',
             reply_markup=keyboards.TRAINING_KEYBOARD
+        )
+    else:
+        await message.answer(
+            'Попробуйте ввести номер телефона еше раз\n'
+            '\nПример: +79081234567'
         )
--- bot/bot/keyboards/__init__.py
+++ bot/bot/keyboards/__init__.py
@@ -1,2 +1,1 @@
-from . import registration
 from .inline import *
