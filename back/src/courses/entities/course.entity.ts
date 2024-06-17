8d8eab835ab2c801f0b6f372cd9b7fa9b171eaa3
balistik <balistiktw@gmail.com>
2024-06-17 11:12:44 +1000

    make rel user and course

--- back/src/courses/entities/course.entity.ts
+++ back/src/courses/entities/course.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { City } from '../../cities/entities/city.entity';
+import { User } from '../../users/entities/user.entity';
 
 @Entity()
 export class Course {
@@ -11,4 +12,7 @@ export class Course {
 
   @ManyToOne(() => City, (city) => city.courses)
   city: City;
+
+  @OneToMany(() => User, (user) => user.course)
+  users: User[];
 }
--- back/src/users/entities/user.entity.ts
+++ back/src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Course } from '../../courses/entities/course.entity';
 
 @Entity()
 export class User {
@@ -16,4 +17,7 @@ export class User {
 
   @Column({ nullable: false })
   phoneNumber: string;
+
+  @ManyToOne(() => Course, (course) => course.users)
+  course: Course;
 }
