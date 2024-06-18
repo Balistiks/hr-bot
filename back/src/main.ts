c5d06ce3f2f62f75545b2bf5c285b691ad83e398
balistik <balistiktw@gmail.com>
2024-06-18 20:28:07 +1000

    add swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AuthGuard } from './auth/auth.guard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,11 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('/api');
   app.useGlobalGuards(new AuthGuard());
+
+  const config = new DocumentBuilder().setTitle('HR-bot').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
