f5a1a6d122979e7f238b63bef904cf7a1c04b971
balistik <balistiktw@gmail.com>
2024-06-25 23:00:29 +1000

    fix errors

--- back/src/answers/answers.module.ts
+++ back/src/answers/answers.module.ts
@@ -3,9 +3,10 @@ import { AnswersService } from './answers.service';
 import { AnswersController } from './answers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Answer } from './entitites/answer.entity';
+import { FilesModule } from '../files/files.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Answer]), AnswersModule],
+  imports: [TypeOrmModule.forFeature([Answer]), FilesModule],
   providers: [AnswersService],
   controllers: [AnswersController],
 })
