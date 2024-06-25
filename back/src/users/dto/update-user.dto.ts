0a0f42b1c5839d3d0d23eca7e3cc19861f84e24b
balistik <balistiktw@gmail.com>
2024-06-26 01:06:00 +1000

    add status to update-user.dto

--- back/src/users/dto/update-user.dto.ts
+++ back/src/users/dto/update-user.dto.ts
@@ -1,6 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsEmpty, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import {
+  IsEmpty,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { Question } from '../../questions/entities/question.entity';
 import { Course } from '../../courses/entities/course.entity';
 import { Answer } from '../../answers/entitites/answer.entity';
@@ -9,6 +15,10 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsNumber()
   @IsNotEmpty()
   id: number;
+
+  @IsOptional()
+  @IsString()
+  status: string;
 
   @IsOptional()
   question: Question;
