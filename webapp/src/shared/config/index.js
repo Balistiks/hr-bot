cf9730553981540f8e611f38183408fc31054905
balistik <balistiktw@gmail.com>
2024-06-29 21:32:40 +1000

    fix env

--- webapp/src/shared/config/index.js
+++ webapp/src/shared/config/index.js
@@ -1,7 +1,7 @@
 export const API_URL = process.env.NODE_ENV === 'development'
-    ? proccess.env.URL + '/api/' :
+    ? import.meta.env.VITE_URL + '/api/' :
     window.location.protocol + '//' + window.location.host + '/api/';
 
 export const API_URL_PATH = (path) => API_URL + path;
 
-export const token = process.env.SECRET_TOKEN;
+export const token = import.meta.env.VITE_SECRET_TOKEN
