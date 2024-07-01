a6b0b2b17bd198e55fb24fd2484d2d3c0b1096b9
Alexandr <xepaerz@mail.ru>
2024-07-01 17:12:07 +1000

    merge in master

--- webapp/src/widgets/vacancy-external/ui/vacancy-external.jsx
+++ webapp/src/widgets/vacancy-external/ui/vacancy-external.jsx
@@ -2,8 +2,24 @@ import {Stack} from "react-bootstrap";
 import {CustomButton} from "@shared/ui/index.js";
 
 import styles from './styles.module.scss';
+import {useEffect} from "react";
+import {useApi} from "@shared/lib/index.js";
 
 export const VacancyExternal = () => {
+    const {data: position, loading: positionLoad, fetchData: fetchPositions} = useApi();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await fetchPositions(`courses/${id}`, 'GET')
+            } catch (error) {
+                console.error(error)
+            }
+        };
+        fetchData();
+    }, []);
+
+
     return (
         <div className={styles.blockVacancyExternal}>
             <Stack gap={4} style={{zIndex: 2, position: 'relative'}}>
@@ -12,27 +28,6 @@ export const VacancyExternal = () => {
                 </CustomButton>
                 <CustomButton typeButton={'white-icon-box'}>
                     Мужской парикмахер <img src={window.location.origin + '/NextButton.svg'}/>
-                </CustomButton>
-                <CustomButton typeButton={'white-icon-box'}>
-                    Администратор <img src={window.location.origin + '/NextButton.svg'}/>
-                </CustomButton>
-                <CustomButton typeButton={'white-icon-box'}>
-                    Администратор <img src={window.location.origin + '/NextButton.svg'}/>
-                </CustomButton>
-                <CustomButton typeButton={'white-icon-box'}>
-                    Администратор <img src={window.location.origin + '/NextButton.svg'}/>
-                </CustomButton>
-                <CustomButton typeButton={'white-icon-box'}>
-                    Администратор <img src={window.location.origin + '/NextButton.svg'}/>
-                </CustomButton>
-                <CustomButton typeButton={'white-icon-box'}>
-                    Администратор <img src={window.location.origin + '/NextButton.svg'}/>
-                </CustomButton>
-                <CustomButton typeButton={'white-icon-box'}>
-                    Администратор <img src={window.location.origin + '/NextButton.svg'}/>
-                </CustomButton>
-                <CustomButton typeButton={'white-icon-box'}>
-                    Администратор <img src={window.location.origin + '/NextButton.svg'}/>
                 </CustomButton>
                 <CustomButton typeButton={'white-icon-box'}>
                     Администратор <img src={window.location.origin + '/NextButton.svg'}/>
