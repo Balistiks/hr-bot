5645aa5e520e7d5d56424bdd1c64d9a1805c5e34
balistik <balistiktw@gmail.com>
2024-07-02 20:16:38 +1000

    remake dto for student

--- back/src/students/dto/update-student.dto.ts
+++ back/src/students/dto/update-student.dto.ts
@@ -1,6 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateStudentDto } from './create-student.dto';
 import { IsBoolean, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { Position } from '../../positions/entities/position.entity';
+import { Question } from '../../questions/entities/question.entity';
 
 export class UpdateStudentDto extends PartialType(CreateStudentDto) {
   @IsNotEmpty()
@@ -10,4 +12,10 @@ export class UpdateStudentDto extends PartialType(CreateStudentDto) {
   @IsOptional()
   @IsBoolean()
   paid: boolean;
+
+  @IsOptional()
+  position: Position;
+
+  @IsOptional()
+  question: Question;
 }
