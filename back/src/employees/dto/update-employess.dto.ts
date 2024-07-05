13f52475ce7e7030861a72a9aa384f268f13f81c
balistik <balistiktw@gmail.com>
2024-07-05 17:04:13 +1000

    fix bug with employee

--- back/src/employees/dto/update-employess.dto.ts
+++ back/src/employees/dto/update-employess.dto.ts
@@ -1,8 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { IsNotEmpty, IsNumber} from 'class-validator';
-import { CreateEmployessDto } from './create-employess.dto';
+import { CreateEmployeeDto } from './create-employee.dto';
 
-export class UpdateEmployessDto extends PartialType(CreateEmployessDto) {
+export class UpdateEmployessDto extends PartialType(CreateEmployeeDto) {
   @IsNotEmpty()
   @IsNumber()
   id: number;
