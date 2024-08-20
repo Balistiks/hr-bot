cd1408995bc7e160921a6debb69141fc10b33ac8
balistik <balistiktw@gmail.com>
2024-08-21 01:09:07 +1000

    add links

--- webapp/src/widgets/footer/ui/footer.jsx
+++ webapp/src/widgets/footer/ui/footer.jsx
@@ -5,10 +5,14 @@ export const Footer = () => {
       <footer className={`${styles.blockFooter}`}>
           <div className={`d-flex justify-content-between align-items-center mx-auto ${styles.containerFooter}`}>
               &copy; ЦирюльникЪ. Все права защищены
-              <div className={'d-flex justify-content-between'}>
-                  <img src={window.location.origin + '/Telegram.svg'}/>
-                  <img src={window.location.origin + '/VK.svg'} className={styles.iconFooterLeft}/>
-              </div>
+            <div className={'d-flex justify-content-between'}>
+              <a href="https://t.me/rabota_v_cirulnike">
+                <img src={window.location.origin + '/Telegram.svg'}/>
+              </a>
+              <a href="https://vk.me/cirulnik_su">
+                <img src={window.location.origin + '/VK.svg'} className={styles.iconFooterLeft}/>
+              </a>
+            </div>
           </div>
       </footer>
   )
