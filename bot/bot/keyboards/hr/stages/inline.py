da3cd678462d91437cd47f66115d9120d7153377
balistik <balistiktw@gmail.com>
2024-08-20 18:58:20 +1000

    answer is not none

--- bot/bot/keyboards/hr/stages/inline.py
+++ bot/bot/keyboards/hr/stages/inline.py
@@ -24,8 +24,7 @@ async def get_data_user(tg_id: int):
     if user_data and 'answers' in user_data:
         questions_and_answers = []
         for answer in user_data['answers']:
-            print(len(answer['text']))
-            if answer['text'] is not None or answer['text'] != '':
+            if len(answer['text']) != 0:
                 print(answer['text'])
                 question = answer['stage']
                 questions_and_answers.append((question, answer['id']))
